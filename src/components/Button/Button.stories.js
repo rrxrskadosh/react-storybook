f026aa453d8b529031e818f1c2898c1afebc02d4
rrxrskadosh <kadoshrr2019@gmail.com>
2022-08-28 17:37:32 -0500

    Created Component and Story Input, Created Story Subscription, Modified and added Sorting Stories in preview.js, Modified Button Component with template and args

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./Button"
 
 export default {
-    title: 'Button',
+    title: 'Form/Button',
     component: Button
 }
 
@@ -10,3 +10,21 @@ export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+const Template = args => <Button { ...args } />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+    variant: 'primary',
+    children: 'Primary Args'
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+...PrimaryA.args,
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+    variant: 'secodary',
+}
